93da65324b4690075992db9ed920f2a8e84c05a4
someshyuvasoft <someshyuvasoft334@gmail.com>
2021-11-18 18:23:54 +0530

    addview

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'welcome#index'
-  # get 'welcome/index'
-  # devise_for :users       
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  get 'welcome/index'
+  devise_for :users
   namespace :api do                                                                            
      namespace :v1 do                                                                         
           devise_scope :user do                                                               
